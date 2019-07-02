db6daceed937a416a1a4dbfbfe25b9a6a5b85c43
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-07-02 15:40:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,45 +2,50 @@ let a = 0;
 let o = 0;
 let b = 0;
 
-let amount= document.getElementById("rsamount");
+let amount = document.getElementById("rsamount");
 function appleAdd() {
     let rupies = document.getElementById("rsamount").value;
 
     a++;
-    fruitAmount= a*10+o*15+b*7;
-    if (fruitAmount > rupies ){
+    fruitAmount = a * 10 + o * 15 + b * 7;
+    if (fruitAmount >= rupies) {
         alert("You have insuficiant balance!");
     }
     else {
         document.getElementById("apple").value = a;
-        document.getElementById("totalamount").value = fruitAmount;
+        document.getElementById("bill").innerHTML = fruitAmount;
     }
 }
 
-function appleLess(){
+function appleLess() {
     let rupies = document.getElementById("rsamount").value;
 
     a--;
-    friutAmount= a*10+o*15+b*7;
-    if (fruitAmount <= 0) {
-        alert("Enter valid number");
+    if (a < 0) {
+        alert("Enter valid number")
     }
     else {
-        document.getElementById("apple").value = a;
-        document.queryseleector("totalamount").value = fruitAmount;
+        fruitAmount = a * 10 + o * 15 + b * 7;
+        if (fruitAmount <= 0) {
+            alert("Enter valid number");
+        }
+        else {
+            document.getElementById("apple").value = a;
+            document.getElementById("bill").innerHTML = fruitAmount;
+        }
     }
 }
 function orangeAdd() {
     let rupies = document.getElementById("rsamount").value;
 
     o++;
-    fruitAmount= a*10+o*15+b*7;
-    if (fruitAmount > rupies ){
+    fruitAmount = a * 10 + o * 15 + b * 7;
+    if (fruitAmount >= rupies) {
         alert("You have insuficiant balance!");
     }
     else {
         document.getElementById("orange").value = o;
-        document.getElementById("totalamount").value = fruitAmount;
+        document.getElementById("bill").innerHTML = fruitAmount;
     }
 }
 
@@ -48,38 +53,48 @@ function orangeLess() {
     let rupies = document.getElementById("rsamount").value;
 
     o--;
-    friutAmount= a*10+o*15+b*7;
-    if (fruitAmount <= 0) {
-        alert("Enter valid number");
+    if (o < 0) {
+        alert("Enter valid number")
     }
     else {
-        document.getElementById("orange").value = o;
-        document.queryseleector("totalamount").value = fruitAmount;
+        fruitAmount = a * 10 + o * 15 + b * 7;
+        if (fruitAmount <= 0) {
+            alert("Enter valid number");
+        }
+        else {
+            document.getElementById("orange").value = o;
+            document.getElementById("bill").innerHTML = fruitAmount;
+        }
     }
 }
 function bananaAdd() {
     let rupies = document.getElementById("rsamount").value;
 
     b++;
-    fruitAmount= a*10+o*15+b*7;
-    if (fruitAmount > rupies ){
+    fruitAmount = a * 10 + o * 15 + b * 7;
+    if (fruitAmount >= rupies) {
         alert("You have insuficiant balance!");
     }
     else {
-        document.getElementById("banana").value =b;
-        document.getElementById("totalamount").value = fruitAmount;
+        document.getElementById("banana").value = b;
+        document.getElementById("bill").innerHTML = fruitAmount;
     }
 }
 function bananaLess() {
     let rupies = document.getElementById("rsamount").value;
 
     b--;
-    friutAmount= a*10+o*15+b*7;
-    if (fruitAmount <= 0) {
-        alert("Enter valid number");
+    if (b < 0) {
+        alert("Enter valid number")
     }
     else {
-        document.getElementById("banana").value = b;
-        document.queryseleector("totalamount").value = fruitAmount;
+        fruitAmount = a * 10 + o * 15 + b * 7;
+        if (fruitAmount <= 0) {
+            alert("Enter valid number");
+        }
+        else {
+            document.getElementById("banana").value = b;
+            document.getElementById("bill").innerHTML = fruitAmount;
+        }
     }
 }
